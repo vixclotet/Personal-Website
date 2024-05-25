42c40dd92b2458a7d00dbe58572830eb757cdc51
Vix <63135033+vix4723@users.noreply.github.com>
2024-05-25 06:59:26 +0200

    updating and uploading...

--- app/page.tsx
+++ app/page.tsx
@@ -14,19 +14,25 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center w-screen min-h-screen overflow-x-hidden overflow-y-auto bg-gradient-to-tl from-cyan-200 to-cyan-100">
       <nav className="my-16 animate-fade-in max-w-4xl mx-auto w-full px-4">
         <ul className="flex items-center justify-center gap-4">
-          <Link
-            href={navigation[0].href}
-            className="text-xl duration-500 text-neutral-700 hover:text-zinc-300 font-mono flex flex-col items-center"
-          >
-            {navigation[0].name}
-          </Link>
-          <img src="/public/favicon.png" alt="Center icon" className="w-16 h-16 mx-16"/>
-          <Link
-            href={navigation[1].href}
-            className="text-xl duration-500 text-neutral-700 hover:text-zinc-300 font-mono flex flex-col items-center"
-          >
-            {navigation[1].name}
-          </Link>
+          <li>
+            <Link
+              href={navigation[0].href}
+              className="text-xl duration-500 text-neutral-700 hover:text-zinc-300 font-mono flex flex-col items-center"
+            >
+              {navigation[0].name}
+            </Link>
+          </li>
+          <li>
+            <img src="favicon.png" alt="Center icon" className="w-24 h-24 mx-16" />
+          </li>
+          <li>
+            <Link
+              href={navigation[1].href}
+              className="text-xl duration-500 text-neutral-700 hover:text-zinc-300 font-mono flex flex-col items-center"
+            >
+              {navigation[1].name}
+            </Link>
+          </li>
         </ul>
       </nav>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
@@ -37,12 +43,20 @@ export default function Home() {
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-sky-900 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         Victoria Clotet
       </h1>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in max-w-4xl mx-auto w-full px-4">
+
+      {/* Basic Introduction Section */}
+      <div className="hidden w-screen space-y-6 h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="my-16 text-center space-y-6 animate-fade-in max-w-4xl mx-auto w-full px-4">
         <h2 className="text-xl text-neutral-900 font-mono">
-          Hi! I'm a 22 year old tech entrepreneur who loves to learn about emerging technologies and build cool things with smart people
+          Hi! I'm a 22 year old tech entrepreneur who loves to learn about emerging technologies and build cool things with smart and ambitious people.
         </h2>
+        <div className="flex flex-col items-center">
+          <img src="SFPic.jpg" alt="SF Pic" className="w-60 h-80 border-4 border-neutral-700" />
+          <h3 className="font-mono">I loooove SF 🌉</h3>
+        </div>
       </div>
+
+      {/* Background Section */}
       <div className="my-16 text-center animate-fade-in max-w-4xl mx-auto w-full px-4 space-y-6">
         <h2 className="text-3xl text-neutral-900 font-mono font-bold">
           Background
@@ -50,14 +64,43 @@ export default function Home() {
         <h3 className="text-lg text-neutral-900 font-mono">
           I'm a recent UVA graduate who double majored in Computer Science and Commerce. I like to build cool things for people and work with other ambitious developers and engineers.
           I like to travel around the EU, USA, and LATAM and learn about the different technologies that lead in each continent. One of my hobbies is photography. I like to take pictures of all of the places I travel to.
-          I have a strong interest on the intersection between AI, Fintech, Content Creation, and SaaS business models. If you are interested and/or passionate about one of these areas, do not be scared to reach out to me!
+          I have a strong interest on the intersection between AI, Fintech, Content Creation, and SaaS business models. <span className="font-bold">If you are interested and/or passionate about one of these areas, do not be scared to reach out to me!</span>
         </h3>
+        <div className="flex flex-col items-center">
+          <img src="travelMap.png" alt="SF Pic" className="w-120 h-80 border-4 border-neutral-700" />
+          <h3 className="font-mono"></h3>
+        </div>
       </div>
-      <div className="my-16 text-center animate-fade-in max-w-4xl mx-auto w-full px-4">
+
+      {/* Technical Contributions/Projects Section */}
+      <div className="my-16 text-center animate-fade-in max-w-4xl mx-auto w-full px-4 space-y-4">
         <h2 className="text-3xl text-neutral-900 font-mono font-bold">
           Technical Contributions/Projects
         </h2>
+        <div className="text-lg text-neutral-900 hover:text-zinc-500 font-mono font-bold">
+          <h3><a href="https://posquotes.com/">PosQuotes</a></h3>
+        </div>
+        <div className="text-lg text-neutral-900 hover:text-zinc-500 font-mono font-bold">
+          <h3><a href="https://x.com/ThePixelFan">AI Pixel Artist Bot</a></h3>
+        </div>
+        <div className="text-lg text-neutral-900 hover:text-zinc-500 font-mono font-bold">
+          <h3><a href="https://github.com/vix4723">Go and Have Fun visiting my Github</a></h3>
+        </div>
+        <div className="text-lg text-neutral-900 hover:text-zinc-500 font-mono font-bold">
+          <h3>More Coming Soon...</h3>
+        </div>
       </div>
+
+      {/* Writing + Thoughts*/}
+      <div className="my-16 text-center animate-fade-in max-w-4xl mx-auto w-full px-4 space-y-4">
+        <div className="inline-block bg-white rounded-full px-6 py-3 shadow-lg">
+          <h2 className="text-3xl text-neutral-900 font-mono font-bold hover:text-zinc-500">
+            <a href="https://x.com/vixclotet">Check my tweets!!</a>
+          </h2>
+        </div>
+      </div>
+
+      {/* Featured Articles Section */}
       <div className="my-16 text-center animate-fade-in space-y-6 max-w-4xl mx-auto w-full px-4">
         <h2 className="text-3xl text-neutral-900 font-mono font-bold underline">
           Featured Articles
